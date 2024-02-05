bf64e59d8bd6ec639fc8f005421d90bc5352e54b
Nilufar <negamberdiyeva478@gmail.com>
2024-02-05 19:04:20 +0500

    1.py commit qoshdm

---
+++ 1.py
@@ -0,0 +1,262 @@
+https://dpaste.org/QUzHQ 
+https://dpaste.org/QUzHQ
+About History New snippet
+Python Expires in: 1 month
+Delete Now
+Raw
+Slim
+https://github.com/abhisheknaiidu/awesome-github-profile-readme?tab=readme-ov-file
+https://github.com/alexandresanlim/Badges4-README.md-Profile
+https://gist.github.com/rxaviers/7360908
+https://github.com/anuraghazra/github-readme-stats
+https://rahuldkjain.github.io/gh-profile-readme-generator/
+
+
+<img align="right" src="https://visitor-badge.laobi.icu/badge?page_id=salesp07.salesp07" />
+
+
+<h1 align="center">
+    <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&center=true&vCenter=true&width=500&height=70&duration=4000&lines=Hi+There!+👋;+I'm+Pedro+Muniz!;" />
+</h1>
+
+<h3 align="center">I am a software engineer from Uzbekistan 🇺🇿</h3>
+
+<br/>
+
+<div align="center">
+ 
+ 🔭 I’m currently working on **a marketplace**
+ 
+ 🌱 I’m currently learning **Docker, Supabase, AWS**
+
+💬 Ask me about **Node.js, React, Firebase... or anything [here](https://github.com/salesp07/salesp07/issues)**
+
+⚡ Fun fact **Game of Thrones Night's Watch cloaks are made from Ikea rugs**
+
+ </div>
+ 
+<div align="center"> 
+  <a href="mailto:pedro.sales.muniz@gmail.com">
+    <img src="https://img.shields.io/badge/Gmail-333333?style=for-the-badge&logo=gmail&logoColor=red" />
+  </a>
+  <a href="https://linkedin.com/in/pedro-sales-muniz" target="_blank">
+    <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank" />
+  </a>
+  <a href="https://salesp07.github.io" target="_blank">
+     <img src="https://img.shields.io/badge/Portfolio-FF5722?style=for-the-badge&logo=todoist&logoColor=white" target="_blank" /> <!-- sqlite, safari, google-chrome are other good icon options -->
+  </a>
+</div>
+
+ <hr/>
+ 
+<h2 align="center">⚒️ Languages-Frameworks-Tools ⚒️</h2>
+<br/>
+<div align="center">
+    <img src="https://skillicons.dev/icons?i=react,bootstrap,mui,html,css,vscode,github,figma,tailwind,git,r" />
+    <img src="https://skillicons.dev/icons?i=nodejs,python,javascript,typescript,express,firebase,mongodb,c,java,nextjs,mysql,flask" /><br>
+</div>
+
+<br/>
+<hr/>
+
+<div align="center">
+  <h2>🐍 My Contributions 🐍</h2>
+  <br>
+  <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/salesp07/salesp07/output/github-contribution-grid-snake.svg" />
+  
+  <br/><br/><br/>
+</div>
+
+<hr/>
+
+<h2 align="center">⚡ Stats ⚡</h2>
+<br>
+<div align=center>
+  <img width=390 src="https://github-readme-streak-stats-salesp07.vercel.app/?user=salesp07&count_private=true&theme=react&border_radius=10" alt="streak stats"/>
+  <img width=390 src="https://github-readme-stats-salesp07.vercel.app/api?username=salesp07&count_private=true&show_icons=true&theme=react&rank_icon=github&border_radius=10" alt="readme stats" />
+  <br/>
+  <img width=325 align="center" src="https://github-readme-stats-salesp07.vercel.app/api/top-langs/?username=salesp07&hide=HTML&langs_count=8&layout=compact&theme=react&border_radius=10&size_weight=0.5&count_weight=0.5&exclude_repo=github-readme-stats" alt="top langs" />
+</div>
+
+<br/><br/>
+
+<hr/>
+
+<br/>
+
+<div align="center">
+<a href='https://ko-fi.com/V7V4RAK9C' target='_blank'><img height='64' style='border:0px;height:64px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+</div>
+
+<br/>
+
+
+
+
+-google translate qo'shish
+-iqtidorli va kafedra tarixini bazadan opkelish
+-saytni yengil ishlashi uchun maksimum cdnlardan voz kechish
+hamda fayllarni maksimum kichraytirish hajmini
+-filterlarni bir qator tekshirib qo'yish
+
+
+/media/uploads/2024/01/30/irisqulov.jpg
+/media/uploads/2024/01/30/hakimjon_abdulhafizov.jpeg
+/media/uploads/2024/01/30/karimov_pazliddin.jpeg
+/media/uploads/2024/01/30/hasanov_xojiakbar.png
+/media/uploads/2024/01/30/aliyev_azamjon.png
+/media/uploads/2024/01/30/orif_ulugbek.png
+
+
+/media/uploads/2024/01/30/xoji_xasanov.png
+
+
+
+
+
+<iframe src="https://onedrive.live.com/embed?resid=997B4F74BFC4D51C%21126&authkey=!AKK60iEmkxYp5vI&em=2" width="402" height="327" frameborder="0" scrolling="no"></iframe>
+
+<p>Hello</p>
+
+
+
+https://youtube.com/playlist?list=PLpDyZ4xZcDg8fRiY6xgsQcDiMjNYJhNjE&si=Q1qb3FouhGe3euoA
+
+https://youtube.com/playlist?list=PLpDyZ4xZcDg8fRiY6xgsQcDiMjNYJhNjE&si=-PSLJKW2BSawX-RC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Copy Snippet
+Edit Snippet
+ Wordwrap
+https://github.com/abhisheknaiidu/awesome-github-profile-readme?tab=readme-ov-file
+https://github.com/alexandresanlim/Badges4-README.md-Profile
+https://gist.github.com/rxaviers/7360908
+https://github.com/anuraghazra/github-readme-stats
+https://rahuldkjain.github.io/gh-profile-readme-generator/
+​
+​
+<img align="right" src="https://visitor-badge.laobi.icu/badge?page_id=salesp07.salesp07" />
+​
+​
+<h1 align="center">
+    <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&center=true&vCenter=true&width=500&height=70&duration=4000&lines=Hi+There!+👋;+I'm+Pedro+Muniz!;" />
+</h1>
+​
+<h3 align="center">I am a software engineer from Uzbekistan 🇺🇿</h3>
+​
+<br/>
+​
+<div align="center">
+ 
+ 🔭 I’m currently working on **a marketplace**
+ 
+ 🌱 I’m currently learning **Docker, Supabase, AWS**
+​
+💬 Ask me about **Node.js, React, Firebase... or anything [here](https://github.com/salesp07/salesp07/issues)**
+​
+⚡ Fun fact **Game of Thrones Night's Watch cloaks are made from Ikea rugs**
+​
+ </div>
+ 
+<div align="center"> 
+  <a href="mailto:pedro.sales.muniz@gmail.com">
+    <img src="https://img.shields.io/badge/Gmail-333333?style=for-the-badge&logo=gmail&logoColor=red" />
+  </a>
+  <a href="https://linkedin.com/in/pedro-sales-muniz" target="_blank">
+    <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank" />
+  </a>
+  <a href="https://salesp07.github.io" target="_blank">
+     <img src="https://img.shields.io/badge/Portfolio-FF5722?style=for-the-badge&logo=todoist&logoColor=white" target="_blank" /> <!-- sqlite, safari, google-chrome are other good icon options -->
+  </a>
+</div>
+​
+ <hr/>
+ 
+<h2 align="center">⚒️ Languages-Frameworks-Tools ⚒️</h2>
+<br/>
+<div align="center">
+    <img src="https://skillicons.dev/icons?i=react,bootstrap,mui,html,css,vscode,github,figma,tailwind,git,r" />
+    <img src="https://skillicons.dev/icons?i=nodejs,python,javascript,typescript,express,firebase,mongodb,c,java,nextjs,mysql,flask" /><br>
+</div>
+​
+<br/>
+<hr/>
+​
+<div align="center">
+  <h2>🐍 My Contributions 🐍</h2>
+  <br>
+  <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/salesp07/salesp07/output/github-contribution-grid-snake.svg" />
+  
+  <br/><br/><br/>
+</div>
+​
+<hr/>
+​
+<h2 align="center">⚡ Stats ⚡</h2>
+<br>
+<div align=center>
+  <img width=390 src="https://github-readme-streak-stats-salesp07.vercel.app/?user=salesp07&count_private=true&theme=react&border_radius=10" alt="streak stats"/>
+  <img width=390 src="https://github-readme-stats-salesp07.vercel.app/api?username=salesp07&count_private=true&show_icons=true&theme=react&rank_icon=github&border_radius=10" alt="readme stats" />
+  <br/>
+  <img width=325 align="center" src="https://github-readme-stats-salesp07.vercel.app/api/top-langs/?username=salesp07&hide=HTML&langs_count=8&layout=compact&theme=react&border_radius=10&size_weight=0.5&count_weight=0.5&exclude_repo=github-readme-stats" alt="top langs" />
+</div>
+​
+<br/><br/>
+​
+<hr/>
+​
+<br/>
+​
+<div align="center">
+<a href='https://ko-fi.com/V7V4RAK9C' target='_blank'><img height='64' style='border:0px;height:64px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+</div>
+​
+<br/>
+​
+​
+​
+​
+-google translate qo'shish
+-iqtidorli va kafedra tarixini bazadan opkelish
+-saytni yengil ishlashi uchun maksimum cdnlardan voz kechish
+hamda fayllarni maksimum kichraytirish hajmini
+-filterlarni bir qator tekshirib qo'yish
+​
+​
+/media/uploads/2024/01/30/irisqulov.jpg
+/media/uploads/2024/01/30/hakimjon_abdulhafizov.jpeg
+/media/uploads/2024/01/30/karimov_pazliddin.jpeg
+/media/uploads/2024/01/30/hasanov_xojiakbar.png
+/media/uploads/2024/01/30/aliyev_azamjon.png
+/media/uploads/2024/01/30/orif_ulugbek.png
+​
+​
+/media/uploads/2024/01/30/xoji_xasanov.png
+​
+​
+​
+​
+​
+<iframe src="https://onedrive.live.com/embed?resid=997B4F74BFC4D51C%21126&authkey=!AKK60iEmkxYp5vI&em=2" width="402" height="327" frameborder="0" scrolling="no"></iframe>
+​
+<p>Hello</p>
+​
+​
+​
+https://youtube.com/playlist?list=PLpDyZ4xZcDg8fRiY6xgsQcDiMjNYJhNjE&si=Q1qb3FouhGe3euoA
+​
+https://youtube.com/playlist?list=PLpDyZ4xZcDg8fRiY6xgsQcDiMjNYJhNjE&si=-PSLJKW2BSawX-RC
